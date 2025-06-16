217ecf9fe5d1b352789489f3a6af9da4df34eff3
DevWonny <cjfdnjs1994@naver.com>
2025-06-16 15:13:34 +0900

    Header Component 구현 진행

--- app/layoutClient.tsx
+++ app/layoutClient.tsx
@@ -1,7 +1,14 @@
 'use client';
+// component
+import Header from '@/components/common/Header';
 
 export default function LayoutClient({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div>{children}123</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 }
