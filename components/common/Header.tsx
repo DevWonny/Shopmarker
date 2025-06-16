5c0d783fc21a972a0f31247391963a63e73b0360
DevWonny <cjfdnjs1994@naver.com>
2025-06-16 15:35:16 +0900

    Header 구현 진행중

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,3 +1,34 @@
+'use client';
+import { useState } from 'react';
+// style
+import '@/styles/common/Header.scss';
+
 export default function Header() {
-  return <div>Header</div>;
+  // state
+  const [search, setSearch] = useState('');
+
+  // function
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log("🚀 ~ onSearch ~ e:", e)
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div className="header-container w-full flex flex-col">
+      <div className="menu-container flex items-center justify-between ">
+        <div className="logo-container">Logo</div>
+        <div className="auth-container">Auth Container</div>
+      </div>
+
+      <div className="search-container w-full flex items-center justify-center">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full"
+          value={search}
+          onChange={onSearch}
+        />
+      </div>
+    </div>
+  );
 }
