acc0a439d435fd15e63518aa0f778d5548e4bb38
DevWonny <cjfdnjs1994@naver.com>
2025-06-17 23:46:51 +0900

    Main Swiper 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
 'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+// style
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="main-wrap">
+      <Swiper
+        className="banner-container"
+        slidesPerView={3}
+        spaceBetween={10}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 10</SwiperSlide>
+      </Swiper>
+      <div className="product-container"></div>
+    </div>
+  );
 }
