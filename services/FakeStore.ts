300221a51b39cc63f12d4682e73251d5c4c4f608
DevWonny <cjfdnjs1994@naver.com>
2025-06-20 20:55:38 +0900

    Product List Service 수정

--- services/FakeStore.ts
+++ services/FakeStore.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 export const GetProductList = async () => {
   const res = await axios.get('https://fakestoreapi.com/products');
+  const { data, status } = res;
 
-  console.log('res', res);
+  if (status !== 200) {
+    console.log('Fake Store GetProductList Error');
+    return;
+  }
+
+  return data;
 };
