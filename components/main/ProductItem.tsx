6dd394c45b882d1a7b28d26f993facd0458c8050
DevWonny <cjfdnjs1994@naver.com>
2025-06-23 19:55:20 +0900

    Product Item 가격 변환 구현

--- components/main/ProductItem.tsx
+++ components/main/ProductItem.tsx
@@ -10,10 +10,21 @@ type ProductItemProps = {
   item: ProductItemType;
 };
 
+// interface
+interface ExchangeRateMap {
+  [currencyCode: string]: number;
+}
+
 // ! props : item -> type check 필요
 export default function ProductItem({ item }: ProductItemProps) {
   // state
   const [isMouseOver, setIsMouseOver] = useState(false);
+
+  // rates
+  const rates = {
+    KRW: 1389.43,
+    USD: 1,
+  };
 
   // function
   const onItemClick = (id: number) => {
@@ -29,9 +40,30 @@ export default function ProductItem({ item }: ProductItemProps) {
     }
   };
 
+  /**
+   * 환율 계산 함수
+   * @params price 가격(number)
+   * @params fromCurrency 기준 통화(USD)
+   * @params toCurrency 대상 통화(KRW)
+   * @params rates 환율 정보 객체
+   * @params returns 변환된 금액
+   */
+  const convertCurrency = (
+    price: number,
+    fromCurrency: string,
+    toCurrency: string,
+    rates: ExchangeRateMap
+  ): number => {
+    if (fromCurrency === toCurrency) return price;
+    const baseToUSD = fromCurrency === 'USD' ? 1 : 1 / rates[fromCurrency];
+    const USDToTarget = toCurrency === 'USD' ? 1 : rates[toCurrency];
+
+    return Math.floor(price * baseToUSD * USDToTarget);
+  };
+
   return (
     <div
-      className="product-item-container flex flex-col justify-between replace"
+      className="product-item-container flex flex-col justify-between relative"
       onClick={() => onItemClick(item.id)}
       onMouseOver={() => onMouseOver(item.id)}
       onMouseLeave={() => setIsMouseOver(false)}
@@ -39,11 +71,11 @@ export default function ProductItem({ item }: ProductItemProps) {
       <img src={item.image} alt="Product Image" />
       {isMouseOver && (
         <div className="desc-container absolute">
-          <p>{item.title}</p>
-          <p>
-            Rating : {item.rating.rate} / {item.rating.count}
+          <p className="title">{item.title}</p>
+          <p className="rating">평점 : {item.rating.rate}</p>
+          <p className="price">
+            가격 : {convertCurrency(item.price, 'USD', 'KRW', rates)}원
           </p>
-          <p>Price : {item.price}</p>
         </div>
       )}
     </div>
