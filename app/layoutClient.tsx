cc1b10951626c66e1126bd2d582f447542dc14aa
DevWonny <cjfdnjs1994@naver.com>
2025-06-24 15:47:39 +0900

    Modal 구조 적용

--- app/layoutClient.tsx
+++ app/layoutClient.tsx
@@ -1,14 +1,16 @@
 'use client';
 // component
 import Header from '@/components/common/Header';
+import BaseModal from '@/components/modal/BaseModal';
 
 export default function LayoutClient({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div>
+    <div className="relative">
       <Header />
       {children}
+      <BaseModal />
     </div>
   );
 }
---
+++ components/modal/BaseModal.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+// style
+import '@/styles/components/modal/BaseModal.scss';
+
+export default function BaseModal() {
+  return (
+    <div className="base-modal-wrap absolute">
+      <p>Base Modal</p>
+    </div>
+  );
+}
