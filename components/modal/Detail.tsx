f71c0ab0e38b80d6448d794ea7910aa4d902829b
DevWonny <cjfdnjs1994@naver.com>
2025-06-24 17:31:42 +0900

    Product Detail Modal 구현 진행중

--- components/modal/Detail.tsx
+++ components/modal/Detail.tsx
@@ -1,15 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 // store
 import { useProduct } from '@/store/Product';
+// style
+import '@/styles/components/modal/Detail.scss';
 
 export default function Detail() {
   const { item } = useProduct();
   return (
     item && (
-      <div className="detail-container">
-        <p>{item.id}</p>
-        <p>{item.description}</p>
+      <div className="detail-container flex ">
+        <img src={item.image} alt="Detail Image" />
+        <div className="description-container flex flex-col">
+          <h1 className="detail-title text-lg">{item.title}</h1>
+          <p className="detail-rate text-sm">Rate : {item.rating.rate}</p>
+          <p className="detail-price text-sm">Price : {item.price}</p>
+          <p className="detail-description ">{item.description}</p>
+        </div>
       </div>
     )
   );
