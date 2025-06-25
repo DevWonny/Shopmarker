6e4e691c2c476ae581aef4599f2735e8d750937f
DevWonny <cjfdnjs1994@naver.com>
2025-06-25 09:45:01 +0900

    Modal Open시 뒷 배경 블러 처리

--- app/layoutClient.tsx
+++ app/layoutClient.tsx
@@ -2,15 +2,26 @@
 // component
 import Header from '@/components/common/Header';
 import BaseModal from '@/components/modal/BaseModal';
+// store
+import { useProduct } from '@/store/Product';
 
 export default function LayoutClient({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const { item } = useProduct();
+
   return (
     <div className="relative">
-      <Header />
-      {children}
-      <BaseModal />
+      <div
+        className={`modal-container w-full h-screen absolute ${item ? `active` : ''}`}
+      >
+        <BaseModal />
+      </div>
+
+      <div className={`layout-container ${item ? 'layout-disabled' : ''}`}>
+        <Header />
+        {children}
+      </div>
     </div>
   );
 }
