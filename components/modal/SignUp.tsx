d96c1aa5d488ce270ccd3378973b95e1e6f06ba3
DevWonny <cjfdnjs1994@naver.com>
2025-07-07 12:28:13 +0900

    Sign In Style 적용

--- components/modal/SignUp.tsx
+++ components/modal/SignUp.tsx
@@ -4,14 +4,19 @@ import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
 // style
-import '@/styles/components/modal/SignUp.scss';
+import '@/styles/components/modal/Auth.scss';
 
 export default function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
   // const [error, setError] = useState('');
   // function
   const onSignUp = async () => {
+    if (password !== passwordCheck) {
+      console.log('Password가 일치하지 않습니다.');
+      return;
+    }
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       console.log('회원가입 완료!');
@@ -21,7 +26,7 @@ export default function SignUp() {
   };
 
   return (
-    <div className="sign-up-form flex col">
+    <div className="sign-up-form flex flex-col">
       <input
         type="text"
         placeholder="ID를 입력하세요."
@@ -33,6 +38,12 @@ export default function SignUp() {
         placeholder="PW를 입력하세요."
         value={password}
         onChange={e => setPassword(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="PW를 한번 더 입력하세요."
+        value={passwordCheck}
+        onChange={e => setPasswordCheck(e.target.value)}
       />
       <button onClick={onSignUp}>회원가입</button>
     </div>
