024ed295fd252777cd424f1c4d24b1921ba4bf8d
DevWonny <cjfdnjs1994@naver.com>
2025-07-08 11:26:47 +0900

    로그아웃 기능 구현

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,12 +2,13 @@
 import { useEffect, useState } from 'react';
 // store
 import { useAuth } from '@/store/Auth';
+import { signOut, getAuth } from 'firebase/auth';
 // style
 import '@/styles/common/Header.scss';
 
 export default function Header() {
   // state
-  const { setIsSignInClick, setIsSignUpClick, user } = useAuth();
+  const { setIsSignInClick, setIsSignUpClick, user, setUser } = useAuth();
   const [isSignIn, setIsSignIn] = useState(false);
 
   // function
@@ -18,6 +19,17 @@ export default function Header() {
     } else if (type === 'signIn') {
       setIsSignInClick(false);
       setIsSignUpClick(true);
+    }
+  };
+
+  const onSignOutClick = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      setIsSignIn(false);
+      setUser(null);
+    } catch (err) {
+      console.log('Sing Out Error! : ', err);
     }
   };
 
@@ -34,7 +46,7 @@ export default function Header() {
       {isSignIn ? (
         <div className="auth-info flex">
           <p>{user.email}</p>
-          <button>Sign Out</button>
+          <button onClick={onSignOutClick}>Sign Out</button>
         </div>
       ) : (
         <div className="auth-button-container flex">
