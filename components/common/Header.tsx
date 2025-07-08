889ecf4e044604a08403bacbdd160b97912adb0c
DevWonny <cjfdnjs1994@naver.com>
2025-07-08 11:11:23 +0900

    Sign In 했을 경우 Header 분기처리

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 // store
 import { useAuth } from '@/store/Auth';
 // style
@@ -6,7 +7,8 @@ import '@/styles/common/Header.scss';
 
 export default function Header() {
   // state
-  const { setIsSignInClick, setIsSignUpClick } = useAuth();
+  const { setIsSignInClick, setIsSignUpClick, user } = useAuth();
+  const [isSignIn, setIsSignIn] = useState(false);
 
   // function
   const onAuthClick = (type: string) => {
@@ -19,12 +21,27 @@ export default function Header() {
     }
   };
 
+  useEffect(() => {
+    if (user) {
+      setIsSignIn(true);
+    } else {
+      setIsSignIn(false);
+    }
+  }, [user]);
+
   return (
     <div className="header-container w-full flex justify-end items-center">
-      <div className="auth-container flex">
-        <button onClick={() => onAuthClick('logIn')}>로그인</button>
-        <button onClick={() => onAuthClick('signIn')}>회원가입</button>
-      </div>
+      {isSignIn ? (
+        <div className="auth-info flex">
+          <p>{user.email}</p>
+          <button>Sign Out</button>
+        </div>
+      ) : (
+        <div className="auth-button-container flex">
+          <button onClick={() => onAuthClick('logIn')}>로그인</button>
+          <button onClick={() => onAuthClick('signIn')}>회원가입</button>
+        </div>
+      )}
     </div>
   );
 }
