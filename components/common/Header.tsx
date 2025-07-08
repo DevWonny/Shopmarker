034b9809275876b486e8af20305d610a292a81c0
DevWonny <cjfdnjs1994@naver.com>
2025-07-08 10:59:40 +0900

    Header Style 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 // store
 import { useAuth } from '@/store/Auth';
 // style
@@ -7,14 +6,9 @@ import '@/styles/common/Header.scss';
 
 export default function Header() {
   // state
-  const [search, setSearch] = useState('');
   const { setIsSignInClick, setIsSignUpClick } = useAuth();
 
   // function
-  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
   const onAuthClick = (type: string) => {
     if (type === 'logIn') {
       console.log('Login Form');
@@ -28,23 +22,10 @@ export default function Header() {
   };
 
   return (
-    <div className="header-container w-full flex flex-col">
-      <div className="menu-container flex items-center justify-between ">
-        <div className="logo-container">Logo</div>
-        <div className="auth-container flex">
-          <button onClick={() => onAuthClick('logIn')}>로그인</button>
-          <button onClick={() => onAuthClick('signIn')}>회원가입</button>
-        </div>
-      </div>
-
-      <div className="search-container w-full flex items-center justify-center">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-full"
-          value={search}
-          onChange={onSearch}
-        />
+    <div className="header-container w-full flex justify-end items-center">
+      <div className="auth-container flex">
+        <button onClick={() => onAuthClick('logIn')}>로그인</button>
+        <button onClick={() => onAuthClick('signIn')}>회원가입</button>
       </div>
     </div>
   );
