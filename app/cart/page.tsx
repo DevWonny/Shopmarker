8da246b78bc595a2fc9908cdfa7d49862701b3d4
DevWonny <cjfdnjs1994@naver.com>
2025-07-22 22:55:56 +0900

    Cart Page Store Binding

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,19 @@
+'use client';
+// store
+import { useProduct } from '@/store/Product';
+
 export default function Cart() {
+  const { cart } = useProduct();
+
   return (
     <div className="card-wrap">
       <p>Card page</p>
+      {cart &&
+        cart.map(item => (
+          <div className="cart-item" key={`cart-page-item-${item.id}`}>
+            {item.title}
+          </div>
+        ))}
     </div>
   );
 }
