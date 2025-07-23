a01de34a825903abd6a962bfb4714cbb7f1ad6b9
DevWonny <cjfdnjs1994@naver.com>
2025-07-23 09:35:47 +0900

    Cart Item Delete 구현

--- components/cart/cartItem.tsx
+++ components/cart/cartItem.tsx
@@ -1,6 +1,8 @@
 'use client';
 // interface
 import { ProductItemType } from '@/types/common/ProductItem';
+// store
+import { useProduct } from '@/store/Product';
 
 // type
 type CartProductItemProps = {
@@ -8,10 +10,18 @@ type CartProductItemProps = {
 };
 
 export default function CartItem({ item }: CartProductItemProps) {
+  const { cart, setCart } = useProduct();
+
+  // function
+  const onCartItemDelete = () => {
+    const { id } = item;
+    setCart(cart.filter(item => item.id !== id));
+  };
+
   return (
     <div className="test-cart-item">
       <p>{item.title}</p>
-      <p>Test 123</p>
+      <button onClick={onCartItemDelete}>Delete</button>
     </div>
   );
 }
