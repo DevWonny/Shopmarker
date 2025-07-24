cfc7e1c7fea570cce8c4ca98f08a0491ca641909
DevWonny <cjfdnjs1994@naver.com>
2025-07-24 10:50:52 +0900

    비 로그인 상태에서 Cart 페이지 접근 금지 구현

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,8 @@
 'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/lib/firebase';
 // store
 import { useProduct } from '@/store/Product';
 // component
@@ -6,7 +10,17 @@ import CartItem from '@/components/cart/cartItem';
 
 export default function Cart() {
   const { cart } = useProduct();
+  const router = useRouter();
+  // useEffect
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (!user) {
+        router.replace('/');
+      }
+    });
 
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="card-wrap">
       <p>Card page</p>
